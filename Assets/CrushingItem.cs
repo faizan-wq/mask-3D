1f84d96e454a2415832e6ba9ebf8ebb781bdd8af
faizan-wq <54429610+faizan-wq@users.noreply.github.com>
2024-02-16 11:02:25 +0500

    update new changes

--- Assets/CrushingItem.cs
+++ Assets/CrushingItem.cs
@@ -6,9 +6,11 @@ public class CrushingItem : MonoBehaviour
 {
     SkinnedMeshRenderer meshShape;
     private float currentValue;
+    private Rigidbody rigidbody;
     private void Awake()
     {
         meshShape = transform.GetChild(0).GetComponent<SkinnedMeshRenderer>();
+        
     }
 
     
@@ -16,9 +18,10 @@ public class CrushingItem : MonoBehaviour
     {
         Debug.Log("Collision:"+collision.gameObject.name);
 
-        if (collision.gameObject==MaskMakingLevel.Instance.hammerController.gameObject)
+        if (collision.gameObject==MaskMakingLevel.Instance.hammerController.gameObject
+            && collision.impulse.magnitude>=2f )
         {
-
+           
 
             Debug.Log("Collided with Hammer");
             currentValue =Mathf.Clamp(currentValue+ 20f,0,100f);
